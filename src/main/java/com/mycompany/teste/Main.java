88a6433bc581e6806a32179e21b280bd1d85fc29
KELVIN MELIM <kelvin.melim@alunos.sc.senac.br>
2022-08-16 21:28:47 -0300

    commit test

---
+++ src/main/java/com/mycompany/teste/Main.java
@@ -0,0 +1,35 @@
+package com.mycompany.teste;
+
+import javax.swing.JOptionPane;
+
+
+public class Main {
+    
+    
+    
+    public static void main(String[] args) {
+    String[][] matriz = new String[15][5];
+    
+    matriz[1][1] = "pergunta1";
+    matriz[1][2] = "";
+    matriz[1][3] = "";
+    matriz[1][4] = "";
+    matriz[1][5] = "";
+    
+    matriz[2][1] = "";
+    matriz[2][2] = "";
+    matriz[2][3] = "";
+    matriz[2][4] = "";
+    matriz[2][5] = "";
+    
+    
+    
+    
+    
+    }
+    
+    
+}
+    
+    
+    
